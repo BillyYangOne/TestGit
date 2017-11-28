2a53e4436efb2f9f8b06925df597dd78d3e2c16c
Billy Yang <BillyYangOne@users.noreply.github.com>
2017-11-28 20:25:08 +0800

    This is the test project first commit.

---
+++ src/com/test/Test.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/**
+ * 测试git
+ * @author billy
+ *
+ */
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("This project is to test git");
+	}
+}
