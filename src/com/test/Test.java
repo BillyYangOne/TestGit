10698ff9d754de2d6e20b82f773c5eb1e6701187
Billy Yang <BillyYangOne@users.noreply.github.com>
2017-11-28 20:31:30 +0800

    Second test

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -8,5 +8,10 @@ package com.test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("This project is to test git");
+		
+		for (int i = 0; i < 100; i++) {
+			
+			System.out.println(System.nanoTime());
+		}
 	}
 }
