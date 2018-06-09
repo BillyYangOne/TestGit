ff3880ff127c01e6d4b3e11cbf269869ce17ffcb
yangle <yangle@kyee.com.cn>
2018-06-09 21:39:28 +0800

    this is a message describing the commit by billy

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -7,6 +7,7 @@ package com.test;
  */
 public class Test {
 	public static void main(String[] args) {
+		
 		System.out.println("This project is to test git");
 		
 		for (int i = 0; i < 100; i++) {
